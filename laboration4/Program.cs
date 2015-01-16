e7c39c0afcc4b157786648879cd40111585f8d1a
jt222ii <jt222ii@student.lnu.se>
2015-01-16 15:14:37 +0100

    iteration 2 implementeringen klar - ska göra tester nu

--- laboration4/Program.cs
+++ laboration4/Program.cs
@@ -15,59 +15,113 @@ namespace laboration4
 
             Console.Clear();
             Teams teams = new Teams();
-         
-            try
+
+            do
             {
-                while (true)
+                try
                 {
-                    int userchoice = int.Parse(userinput("Välkommen. Vad vill du göra? \n1. registrera lag \n2. Se rankingen \n3. hantera lag \n4. Räkna poäng  \n0. stäng av"));
-                    switch (userchoice)
+                    while (true)
                     {
-                        case 0:
-                            Console.WriteLine("stänger av");
-                            return;
-                        case 1:
-                            while (true)
-                            {
-                                try
+                        int userchoice = int.Parse(userinput("Välkommen. Vad vill du göra? \n1. registrera lag \n2. Se rankingen \n3. hantera lag \n4. Räkna poäng  \n0. stäng av"));
+                        switch (userchoice)
+                        {
+                            case 0:
+                                Console.WriteLine("stänger av");
+                                return;
+                            case 1:
+                                while (true)
                                 {
+                                    try
+                                    {
 
-                                    string userInput = userinput("Namn på laget?(3-12 bokstäver)");
-                                    teams.addTeam(userInput);
-                                    Console.WriteLine("Lag registrerat");
+                                        string userInput = userinput("Namn på laget?(3-12 bokstäver)");
+                                        teams.addTeam(userInput);
+                                        Console.WriteLine("Lag registrerat");
+                                        break;
+
+                                    }
+                                    catch (ArgumentOutOfRangeException) { Console.WriteLine("du skrev inte ett namn som är minst 3 bokstäver och högst 12 bokstäver långt"); }
+                                    catch (ArgumentException)
+                                    {
+                                        Console.WriteLine("Ett lag med det namnet finns redan!");
+                                        break;
+                                    }
+                                    catch (Exception)
+                                    {
+                                        Console.WriteLine("Det finns redan maximalt antal lag i turneringen. Tyvärr!");
+                                        break;
+                                    }
+
+                                }
+
+                                break;
+                            case 2: //Se lagen och dess poäng
+                                teams.showLeaderboard(); 
+                                break;
+                            case 3: //Ta bort/byta namn på lag
+                                //visa listan på lag
+
+                                teams.showTeams();
+                                int chosenteam = 0;
+                                if (teams.teamsAdded != 0)
+                                {
+                                    chosenteam = int.Parse(userinput("Välj ett lag!"));
+                                    if (teams._teamNames[chosenteam - 1] == null)
+                                    {
+                                        Console.WriteLine("DU VALDE INTE ETT LAG");
+                                        Console.ReadKey();
+                                        break; 
+                                    }
+                                }
+                                else
+                                {
                                     break;
-
                                 }
-                                catch (ArgumentOutOfRangeException) { Console.WriteLine("du skrev inte ett namn som är minst 3 bokstäver och högst 12 bokstäver långt"); }
-                                catch (ArgumentException) { Console.WriteLine("Ett lag med det namnet finns redan!");
+                                switch (int.Parse(userinput("Vad vill du göra?\n1. ta bort lag 2. byta namn 0. avbryt")))
+                                {
+                                    case 0:
+                                        break;
+                                    case 1:
+                                        teams.removeTeam(chosenteam - 1);
+                                        break;
+                                    case 2:
+                                        try
+                                        {
+                                            string newname = userinput("Skriv in det nya namnet!");
+                                            teams.changeName((chosenteam - 1), newname);
+                                            break;
+                                        }
+                                        catch (ArgumentOutOfRangeException) 
+                                        { 
+                                            Console.WriteLine("du skrev inte ett namn som är minst 3 bokstäver och högst 12 bokstäver långt");
+                                            break;
+                                        }
+                                        catch (ArgumentException)
+                                        {
+                                            Console.WriteLine("Ett lag med det namnet finns redan!");
+                                            break;
+                                        }
+                                        catch (Exception)
+                                        {
+                                            Console.WriteLine("Det finns redan maximalt antal lag i turneringen. Tyvärr!");
+                                            break;
+                                        }
+                                }
                                 break;
-                                }
-                                catch (Exception) { Console.WriteLine("Det finns redan maximalt antal lag i turneringen. Tyvärr!");
-                                break;
-                                }
 
-                            }
-                            
-                            break;
-                        case 2: //Se lagen och dess poäng
-                            teams.showTeams();
-                            break;
-                        case 3: //Ta bort/byta namn på lag
-                            //visa listan på lag
-                            //
-                            break;
-                        case 4: //Välja lag och lägga till poängen till laget.
-                            break;
-                        default:
-                            Console.WriteLine("du valde inget av alternativen...");
-                            break;
-                    } 
+                            case 4: //Välja lag och lägga till poängen till laget.
+                                break;
+                            default:
+                                Console.WriteLine("du valde inget av alternativen...");
+                                break;
+                        }
+                    }
                 }
-            }
-            catch
-            {
-                Console.WriteLine("Du skrev inte in ett nummer");
-            }
+                catch
+                {
+                    Console.WriteLine("Du skrev inte in ett nummer");
+                } 
+            } while (true);
 
         }
 
--- laboration4/Teams.cs
+++ laboration4/Teams.cs
@@ -34,9 +34,77 @@ namespace laboration4
             //Console.WriteLine( _teamNames[teamsAdded] + ": " + _teamPoints[teamsAdded] );
         }
 
-        public void removeTeam()
+        public void removeTeam(int chosenteam)
         {
+            
+            string[] tempArray = _teamNames.Where(w => w != _teamNames[chosenteam]).ToArray();
+            int[] tempPoints = _teamPoints.Where(w => w != _teamPoints[chosenteam]).ToArray();
+            int tempspot = 0;
+            for (int i = 0; i < _teamNames.Length-1; i++)
+            {
+                if (tempArray[i] != null)
+                {
+                    _teamNames[tempspot] = tempArray[i];
+                    _teamPoints[tempspot] = tempPoints[i];
+                    teamsAdded = teamsAdded - 1;
+                    tempspot += 1;
+                }
+                else if(tempArray[i] == null && _teamNames[i] != null)
+                {
+                    _teamNames[i] = null;
+                    _teamPoints[tempspot] = 0;
+                    teamsAdded = teamsAdded - 1;
+                    tempspot += 1;
+                }
+                else 
+                    tempspot += 1;
+            }
+            
+        }
+        public void changeName(int chosenteam, string name)
+        {           
+           
+            if (name.Length > 12 || name.Length < 3)
+            { throw new ArgumentOutOfRangeException(); }
+            if (teamsAdded == _teamNames.Length)
+            { throw new Exception(); }
+            for (int i = 0; i < _teamNames.Length; i++)
+            {
+                if (name == _teamNames[i])
+                {
+                    throw new ArgumentException();
+                }
+            }
+            _teamNames[chosenteam] = name;
+        }
 
+        public void showLeaderboard()
+        {
+            int nummer = 1;
+            if (teamsAdded > 0)
+            {
+                int[] points = new int[15];
+                string[] names = new string[15];
+                Array.Copy(_teamNames, names, 15);
+                Array.Copy(_teamPoints, points, 15);
+                Array.Sort(points, names);
+                Array.Reverse(points);
+                Array.Reverse(names);
+
+                for (int i = 0; i < names.Length; i++)
+                {
+                    if (names[i] != null)
+                    {
+                        Console.WriteLine("{2}. {1}: {0}", points[i], names[i], nummer);
+                        nummer += 1;
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("Det finns inga lag registrerade här");
+                Console.ReadKey();
+            }
         }
 
         public void showTeams()
@@ -44,22 +112,20 @@ namespace laboration4
             int nummer = 1;
             if (teamsAdded > 0)
             {
-                
-                Array.Sort(_teamPoints, _teamNames);
-                Array.Reverse(_teamPoints);
-                Array.Reverse(_teamNames);
-                
                 for (int i = 0; i < _teamNames.Length; i++)
                 {
                     if (_teamNames[i] != null)
                     {
-                        Console.WriteLine("{2}. {1}: {0}", _teamPoints[i], _teamNames[i], nummer);
+                        Console.WriteLine("{1}. {0}", _teamNames[i], nummer);
                         nummer += 1;
                     }
                 }
             }
             else
+            {
                 Console.WriteLine("Det finns inga lag registrerade här");
+                Console.ReadKey();
+            }
         }
 
     }
